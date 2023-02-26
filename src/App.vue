3b988d2da5721c69c991b1d51c7a269c307bd8c9
Chirkov Petro <tchirkov.petter@gmail.com>
2023-02-26 11:11:42 +0200

    removed sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="wrapper">
     <main class="main">
-      <Sidebar />
-      <!-- <Navbar /> -->
       <div class="main__content">
+        <Navbar />
         <RouterView />
       </div>
     </main>
@@ -12,7 +11,6 @@
 
 <script setup>
 import Navbar from './components/Navbar.vue';
-import Sidebar from './components/Sidebar.vue';
 
 </script>
 
@@ -29,11 +27,11 @@ import Sidebar from './components/Sidebar.vue';
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding-inline: 16px;
 
   &__content {
     max-width: 640px;
     width: 100%;
-    margin-left: 200px;
   }
 }
 </style>
